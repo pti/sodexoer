0db04db41b6f166d4ab344f261e959b3220e8b3f
Pekko Tiitto <tiittop@gmail.com>
2019-03-01 20:34:03 +0200

    Fixed parsing bugs in antellizer.

--- antellizer.js
+++ antellizer.js
@@ -25,69 +25,63 @@ async function loadMenu(url) {
     }
 
     let menu = {}
-    let weekdayItems = undefined
-
     const doc = cheerio.load(html)
-    let elem = doc('div .lunch-menu-language').first().find('h3').first()
 
-    while (true) {
+    doc('div .lunch-menu-language').first().find('h3').each(function (index, element) {
+        let elem = doc(this)
+        let wd = undefined
 
-        if (elem.is('h3') && elem.text() !== undefined) {
-            let wd = weekdayMap.get(elem.text().replace(/^(\w+).*/, '$1'))
-
-            if (wd !== undefined) {
-                weekdayItems = []
-                menu[wd] = weekdayItems
-            }
-
-        } else if (elem.is('ul')) {
-
-            elem.find('li').each((i, elem) => {
-                const li = doc(elem)
-
-                if (li.attr('class') === 'menu-item-category') {
-                    const type = li.find('strong').first().text().trim()
-
-                } else {
-                    let txt = li.text()
-                        .trim()
-                        .replace(/\s{2,}/g, ' ')
-                        .replace(/ ,/, ',')
-
-                    // Each menu item can contain multiple pieces and each can have their own set of properties (L, G and such).
-                    // Split the item to pieces and create an object for each that defines the text part and the properties.
-                    // HTML generator can the apply proper styling for the separated properties.
-                    const pieces = txt.split(/\(((?:(?:Veg|[A-Z*])(?:, )?)+)\)/)
-                    let parts = []
-
-                    for (let i = 0; i < pieces.length; i += 2) {
-                        let properties = []
-                        let text = pieces[i]
-
-                        if (text.length == 0) {
-                            continue
-                        }
-
-                        if (i < pieces.length - 1) {
-                            properties = pieces[i + 1].split(/, ?/)
-                        }
-
-                        parts.push({
-                            text: text.trim().replace(/^, /, ''),
-                            properties: properties
-                        })
-                    }
-
-                    weekdayItems.push({parts: parts})
-                }
-            })
-
-        } else {
-            break;
+        if (elem.text() !== undefined) {
+            wd = weekdayMap.get(elem.text().replace(/^(\w+).*/, '$1'))
         }
 
-        elem = elem.next()
-    }
+        if (wd === undefined) {
+            return
+        }
+
+        let weekdayItems = []
+        menu[wd] = weekdayItems
+
+        elem.next('ul').find('li').each((i, elem) => {
+            const li = doc(elem)
+
+            if (li.attr('class') === 'menu-item-category') {
+                const type = li.find('strong').first().text().trim()
+
+            } else {
+                let txt = li.text()
+                    .trim()
+                    .replace(/\s{2,}/g, ' ')
+                    .replace(/ ,/, ',')
+
+                // Each menu item can contain multiple pieces and each can have their own set of properties (L, G and such).
+                // Split the item to pieces and create an object for each that defines the text part and the properties.
+                // HTML generator can the apply proper styling for the separated properties.
+                const pieces = txt.split(/\(((?:(?:Veg|[A-Z*])(?:, )?)+)\)/)
+                let parts = []
+
+                for (let i = 0; i < pieces.length; i += 2) {
+                    let properties = []
+                    let text = pieces[i]
+
+                    if (text.length == 0) {
+                        continue
+                    }
+
+                    if (i < pieces.length - 1) {
+                        properties = pieces[i + 1].split(/, ?/)
+                    }
+
+                    parts.push({
+                        text: text.trim().replace(/^, /, ''),
+                        properties: properties
+                    })
+                }
+
+                weekdayItems.push({parts: parts})
+            }
+        })
+    })
 
     return {
         menus: menu
